1f43406edca258e81ef2af3fdac42cc28bdfd102
Mahmoud Akl <mahmoud.akl@gmail.com>
2018-05-17 11:34:51 +0200

    Added 10 hard coded joint configurations

--- arm_control.py
+++ arm_control.py
@@ -1,21 +1,20 @@
 # Imported Python Transfer Function
 import numpy as np
-import collections
 
 # APPROACH
-approach_tray_conf = np.array([0.0, 0.5, -2.0, 0.0, 0.0, 0.0])
+approach_tray_conf = np.array([0.0, 0.3, -1.0, 0.0, 0.0, 0.0])
 
 # GRASP
-grasp_conf_0 = np.array([0.0, 0.5, -2.0, 0.0, 0.0, 0.0])
-grasp_conf_1 = np.array([0.05, 0.5, -2.0, 0.0, 0.0, 0.0])
-grasp_conf_2 = np.array([0.05, 0.5, -2.0, 0.0, 0.0, 0.0])
-grasp_conf_3 = np.array([0.05, 0.5, -2.0, 0.0, 0.0, 0.0])
-grasp_conf_4 = np.array([0.05, 0.5, -2.0, 0.0, 0.0, 0.0])
-grasp_conf_5 = np.array([0.05, 0.5, -2.0, 0.0, 0.0, 0.0])
-grasp_conf_6 = np.array([0.05, 0.5, -2.0, 0.0, 0.0, 0.0])
-grasp_conf_7 = np.array([0.05, 0.5, -2.0, 0.0, 0.0, 0.0])
-grasp_conf_8 = np.array([0.05, 0.5, -2.0, 0.0, 0.0, 0.0])
-grasp_conf_9 = np.array([0.05, 0.5, -2.0, 0.0, 0.0, 0.0])
+grasp_conf_0 = np.array([0.6, 0.6, -2.4, 0.0, 0.0, 0.0])
+grasp_conf_1 = np.array([0.4, 0.5, -2.2, 0.0, -0.3, 1.0])
+grasp_conf_2 = np.array([0.3, 0.9, -1.6, 1.0, 0.0, 0.0])
+grasp_conf_3 = np.array([-0.15, 0.75, -1.5, -0.2, 0.0, 0.2])
+grasp_conf_4 = np.array([0.3, 0.6, -2.2, 0.0, -0.3, 0.5])
+grasp_conf_5 = np.array([0.0, 0.8, -2.0, 0.3, 0.3, 0.0])
+grasp_conf_6 = np.array([0.6, 0.7, -2.0, 1.0, 0.0, 0.7])
+grasp_conf_7 = np.array([0.0, 0.7, -2.4, 0.2, 0.5, 0.0])
+grasp_conf_8 = np.array([0.3, 0.6, -2.0, 0.6, -0.5, 0.0])
+grasp_conf_9 = np.array([0.35, 1.0, -1.5, 0.7, 0.0, 0.7])
 
 # DISPOSE
 dispose_tray_conf = np.array([-0.7, 0.3, -1.5, 0.0, 0.0, 0.0])
@@ -32,15 +31,24 @@ reset_conf = np.zeros(6)
 @nrp.MapRobotSubscriber('command', Topic('/arm_robot/arm_commands', std_msgs.msg.String))
 @nrp.MapVariable("last_command_executed", initial_value=None)
 @nrp.MapVariable("approach_tray_conf", initial_value=approach_tray_conf)
+@nrp.MapVariable("grasp_conf_0", initial_value=grasp_conf_0)
 @nrp.MapVariable("grasp_conf_1", initial_value=grasp_conf_1)
 @nrp.MapVariable("grasp_conf_2", initial_value=grasp_conf_2)
-@nrp.MapVariable("dispose_tray_conf", initial_value=dispose_blue_conf)
+@nrp.MapVariable("grasp_conf_3", initial_value=grasp_conf_3)
+@nrp.MapVariable("grasp_conf_4", initial_value=grasp_conf_4)
+@nrp.MapVariable("grasp_conf_5", initial_value=grasp_conf_5)
+@nrp.MapVariable("grasp_conf_6", initial_value=grasp_conf_6)
+@nrp.MapVariable("grasp_conf_7", initial_value=grasp_conf_7)
+@nrp.MapVariable("grasp_conf_8", initial_value=grasp_conf_8)
+@nrp.MapVariable("grasp_conf_9", initial_value=grasp_conf_9)
+@nrp.MapVariable("dispose_tray_conf", initial_value=dispose_tray_conf)
 @nrp.MapVariable("reset_conf", initial_value=reset_conf)
 @nrp.Neuron2Robot()
 def arm_control(t, command, last_command_executed, approach_tray_conf,
-                grasp_conf_1, grasp_conf_2, dispose_tray_conf, reset_conf,
-                topic_arm_1, topic_arm_2, topic_arm_3, topic_arm_4,
-                topic_arm_5, topic_arm_6):
+                grasp_conf_0, grasp_conf_1, grasp_conf_2, grasp_conf_3, grasp_conf_4,
+                grasp_conf_5, grasp_conf_6, grasp_conf_7, grasp_conf_8, grasp_conf_9,
+                dispose_tray_conf, reset_conf, topic_arm_1, topic_arm_2, topic_arm_3,
+                topic_arm_4, topic_arm_5, topic_arm_6):
 
     def send_joint_config(topics_list, config_list):
         for topic, value in zip(topics_list, config_list):
@@ -52,15 +60,18 @@ def arm_control(t, command, last_command_executed, approach_tray_conf,
         command_str = command.value.data
     if command_str == last_command_executed.value:
         return
+
+    import collections
     clientLogger.info("ARM received: {}".format(command_str))
     topics_arm = [topic_arm_1, topic_arm_2, topic_arm_3, topic_arm_4, topic_arm_5, topic_arm_6]
     commands_confs = collections.defaultdict(None, {
             "APPROACH": {"TRAY": approach_tray_conf.value},
-            "GRASP": {"1": grasp_conf_1.value, "2": grasp_conf_2.value},
+            "GRASP": {"0": grasp_conf_0.value, "1": grasp_conf_1.value, "2": grasp_conf_2.value, "3": grasp_conf_3.value,
+                      "4": grasp_conf_4.value, "5": grasp_conf_5.value, "6": grasp_conf_6.value, "7": grasp_conf_7.value,
+                      "8": grasp_conf_8.value, "9": grasp_conf_9.value},
             "DISPOSE": {"TRAY": dispose_tray_conf.value},
             "RESET": reset_conf.value
-        }
-    )
+        })
 
     # parse command
     split_command = command_str.split('_')
--- hand_control.py
+++ hand_control.py
@@ -65,13 +65,17 @@ def hand_control(t, command, last_command_executed,
                     topic_thumb_medial,
                     Thumb_Opposition, topic_Thumb_Opposition,
                     topic_palm):
+
     if command.value is None:
         return
     else:
         command_str = command.value.data
+
     if command_str == last_command_executed.value:
         return
+
     clientLogger.info("HAND received: {}".format(command_str))
+
     # Index
     def flex_index(RANGE_MAX, FACTORS, grasp):
         Index_Proximal.value = RANGE_MAX["Index_Proximal"] * FACTORS["Index_Proximal"] * grasp
@@ -80,6 +84,7 @@ def hand_control(t, command, last_command_executed,
         topic_Index_Medial.send_message(std_msgs.msg.Float64(Index_Medial.value))
         # distal joint mimics Medial with a 1.045 coefficient
         topic_index_distal.send_message(std_msgs.msg.Float64(Index_Medial.value * 1.045))
+
     # Middle
     def flex_middle(RANGE_MAX, FACTORS, grasp):
         Middle_Proximal.value = RANGE_MAX["Middle_Proximal"] * FACTORS["Middle_Proximal"] * grasp
@@ -88,6 +93,7 @@ def hand_control(t, command, last_command_executed,
         topic_Middle_Medial.send_message(std_msgs.msg.Float64(Middle_Medial.value))
         middle_distal = Middle_Medial.value * 1.0434
         topic_middle_distal.send_message(std_msgs.msg.Float64(middle_distal))
+
     # Ring
     def flex_ring(RANGE_MAX, FACTORS, grasp):
         Ring_Proximal.value = RANGE_MAX["Ring_Proximal"] * FACTORS["Ring_Proximal"] * grasp
@@ -96,6 +102,7 @@ def hand_control(t, command, last_command_executed,
         topic_ring_medial.send_message(std_msgs.msg.Float64(ring_medial))
         ring_distal = Ring_Proximal.value * 1.42307
         topic_ring_distal.send_message(std_msgs.msg.Float64(ring_distal))
+
     # Pinky
     def flex_pinky(RANGE_MAX, FACTORS, grasp):
         Pinky_Proximal.value = RANGE_MAX["Pinky_Proximal"] * FACTORS["Pinky_Proximal"] * grasp
@@ -104,6 +111,7 @@ def hand_control(t, command, last_command_executed,
         topic_pinky_medial.send_message(std_msgs.msg.Float64(pinky_medial))
         pinky_distal = Pinky_Proximal.value * 1.42307
         topic_pinky_distal.send_message(std_msgs.msg.Float64(pinky_distal))
+
     # Thumb
     def flex_thumb(RANGE_MAX, FACTORS, grasp):
         Thumb_Opposition.value = RANGE_MAX["Thumb_Opposition"] * FACTORS["Thumb_Opposition"] * grasp
@@ -116,6 +124,7 @@ def hand_control(t, command, last_command_executed,
         topic_thumb_medial.send_message(std_msgs.msg.Float64(thumb_medial))
         # Palm
         topic_palm.send_message(std_msgs.msg.Float64(Thumb_Opposition.value * 0.3))
+
     do_flex = {
         "Index": flex_index,
         "Middle": flex_middle,
@@ -123,6 +132,7 @@ def hand_control(t, command, last_command_executed,
         "Pinky": flex_pinky,
         "Thumb": flex_thumb
     }
+
     def parse_grasping_command(cmd):
         do_grasp = None
         if cmd == "GRASP":
@@ -130,6 +140,7 @@ def hand_control(t, command, last_command_executed,
         elif cmd == "RELEASE":
             do_grasp = 0
         return do_grasp
+        
     grasp = parse_grasping_command(command_str)
     if grasp is not None:
         last_command_executed.value = command_str
