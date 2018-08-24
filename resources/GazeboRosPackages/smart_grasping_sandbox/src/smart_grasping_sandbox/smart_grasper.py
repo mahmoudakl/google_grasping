9cf5aab5d716d40e46f46516a43a50b723aa5b88
Luchta <peer.lucas@tum.de>
2018-08-24 10:12:49 +0200

    Collisions working, floor added, added start_grasp to ur10.srdf and call function "start()" to smart_grasper.py to initialise grasping pose. <

--- resources/GazeboRosPackages/smart_grasping_sandbox/src/smart_grasping_sandbox/smart_grasper.py
+++ resources/GazeboRosPackages/smart_grasping_sandbox/src/smart_grasping_sandbox/smart_grasper.py
@@ -4,8 +4,8 @@
 import rospy
 from std_srvs.srv import Empty
 from gazebo_msgs.srv import GetModelState, SetModelConfiguration, DeleteModel, \
-    SpawnModel
-from geometry_msgs.msg import Pose, Point
+    SpawnEntity
+from geometry_msgs.msg import Pose
 from controller_manager_msgs.srv import SwitchController, SwitchControllerRequest
 from moveit_msgs.msg import PlanningScene, PlanningSceneComponents
 from moveit_msgs.srv import GetPlanningScene
@@ -15,7 +15,6 @@ from control_msgs.msg import FollowJointTrajectoryAction, \
     FollowJointTrajectoryGoal
 from sensor_msgs.msg import JointState
 from trajectory_msgs.msg import JointTrajectoryPoint
-from tf2_msgs.msg import TFMessage
 from tf.transformations import quaternion_from_euler
 from math import pi
 from copy import deepcopy
@@ -33,7 +32,6 @@ class SmartGrasper(object):
     """
 
     __last_joint_state = None
-    __object_pose = None
     __current_model_name = "cricket_ball"
     __path_to_models = "/root/.gazebo/models/"
 
@@ -48,8 +46,6 @@ class SmartGrasper(object):
         # pdb.set_trace()
         self.__joint_state_sub = rospy.Subscriber("/joint_states", JointState,
                                                   self.__joint_state_cb, queue_size=1)
-
-        self.__tf_subscriber = rospy.Subscriber('tf', TFMessage, self.__tf_callback, queue_size=1)
 
         rospy.wait_for_service("/gazebo/get_model_state", 10.0)
         rospy.wait_for_service("/gazebo/reset_world", 10.0)
@@ -67,8 +63,8 @@ class SmartGrasper(object):
 
         rospy.wait_for_service("/gazebo/delete_model")
         self.__delete_model = rospy.ServiceProxy("/gazebo/delete_model", DeleteModel)
-        rospy.wait_for_service("/gazebo/spawn_sdf_model")
-        self.__spawn_model = rospy.ServiceProxy("/gazebo/spawn_sdf_model", SpawnModel)
+        rospy.wait_for_service("/gazebo/spawn_sdf_entity")
+        self.__spawn_model = rospy.ServiceProxy("/gazebo/spawn_sdf_entity", SpawnEntity)
 
         rospy.wait_for_service('/get_planning_scene', 10.0)
         self.__get_planning_scene = rospy.ServiceProxy('/get_planning_scene', GetPlanningScene)
@@ -125,8 +121,7 @@ class SmartGrasper(object):
         Gets the pose of the ball in the world frame.
         @return The pose of the ball.
         """
-        # return self.__get_pose_srv.call(self.__current_model_name, "world").pose
-        return self.__object_pose
+        return self.__get_pose_srv.call(self.__current_model_name, "world").pose
 
     def get_tip_pose(self):
         """
@@ -223,6 +218,17 @@ class SmartGrasper(object):
         self.hand_commander.set_named_target("open")
         plan = self.hand_commander.plan()
         if not self.hand_commander.execute(plan, wait=True):
+            return False
+        return True
+
+    def start(self):
+        """
+        puts the arm into starting position.
+        @return True on success
+        """
+        self.arm_commander.set_named_target("start_grasp")
+        plan = self.arm_commander.plan()
+        if not self.arm_commander.execute(plan, wait=True):
             return False
         return True
 
@@ -407,12 +413,3 @@ class SmartGrasper(object):
 
     def __joint_state_cb(self, msg):
         self.__last_joint_state = msg
-
-    def __tf_callback(self, data):
-        for (ids,transform) in enumerate(data.transforms):
-           #rospy.loginfo(transform.child_frame_id)
-           if transform.child_frame_id=="cricket_ball__link":
-              t = transform.transform
-
-              self.__object_pose = Pose(position=Point(t.translation.x,t.translation.y,t.translation.z), orientation=t.rotation)
-              # rospy.loginfo(str(transform.transform.translation))
