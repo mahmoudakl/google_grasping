20ba77715bdb62bf6228c2f1f11e9fd756a42a3a
Mahmoud Akl <mahmoud.akl@epfl.ch>
2018-09-03 17:50:59 +0200

    modified deploy_experiment script, cleaned up environment sdf, fixed paths in GazeboRosPackages

--- deploy_experiment.sh
+++ deploy_experiment.sh
@@ -1,62 +1,63 @@
 #!/bin/bash  
 
+# This bash script copies the necessary files to your local NRP repositories in order
+# for the experiment to be clonable and runnable on your local setup.
+
 echo "Starting to Deploy"  
 git submodule init
 git submodule update --recursive
 echo "submodule initalized"
+
+# GazeboRosPackages
 if [ "$1" = "" ] || [ "$1" = "g" ] ; then
  echo "copying packages"
- mkdir $HBP/GazeboRosPackages/src/google_grasping_packages
- if [ $? -ne 0 ] ; then
+ if [ -d "$HBP/GazeboRosPackages/src/google_grasping_packages"  ] ; then
   echo "overwriting existing folder" 
   rm -r $HBP/GazeboRosPackages/src/google_grasping_packages
-  mkdir $HBP/GazeboRosPackages/src/google_grasping_packages
  fi
+ mkdir $HBP/GazeboRosPackages/src/google_grasping_packages
  cp resources/GazeboRosPackages/* $HBP/GazeboRosPackages/src/google_grasping_packages -a
 fi
-#
+
+# Models
 if [ "$1" = "" ] || [ "$1" = "m" ] ; then
  echo "copying models"
- mkdir $HBP/Models/google_grasping_models
- if [ $? -ne 0 ] ; then
+ if [ -d "$HBP/Models/google_grasping_models" ] ; then
   echo "overwriting existing folder" 
   rm -r $HBP/Models/google_grasping_models
-  mkdir $HBP/Models/google_grasping_models
  fi
+ mkdir $HBP/Models/google_grasping_models
  cp resources/Models/* $HBP/Models/google_grasping_models -a
- cp ur_description/model.sdf $HBP/Models/google_grasping_models/ur_description/model.sdf
- cp google_virtuallab.sdf $HBP/Models/google_grasping_models/google_virtuallab/google_virtuallab.sdf	
 fi
-#
+
+# Experiment
 if [ "$1" = "" ] || [ "$1" = "e" ] ; then
  echo "copying experiment"
- mkdir $HBP/Experiments/GoogleGrasping
- if [ $? -ne 0 ] ; then
+ if [ -d "$HBP/Experiments/GoogleGrasping" ] ; then
   echo "overwriting existing folder" 
   rm -r $HBP/Experiments/GoogleGrasping
-  mkdir $HBP/Experiments/GoogleGrasping
  fi
- cp bibi_configuration.bibi $HBP/Experiments/GoogleGrasping/bibi_configuration.bibi
- cp experiment_configuration.exc $HBP/Experiments/GoogleGrasping/experiment_configuration.exc
- #cp GoogleGrasping.3ds $HBP/Experiments/GoogleGrasping/GoogleGrasping.3ds
- cp GoogleGrasping.jpg $HBP/Experiments/GoogleGrasping/GoogleGrasping.jpg
- cp GoogleGrasping.uis $HBP/Experiments/GoogleGrasping/GoogleGrasping.uis
- cp load_param.launch $HBP/Experiments/GoogleGrasping/load_param.launch
- cp idle_brain.py $HBP/Experiments/GoogleGrasping/idle_brain.py
+ mkdir $HBP/Experiments/GoogleGrasping
+ cp {*.bibi,*.exc,*.json,*.jpg,*.png,*.ini,*.uis,*.launch,*.py} $HBP/Experiments/GoogleGrasping
 fi
-#
+# rename experiment name to distinguish repository from cloned experiments
+sed -e 's/ - Repository//g' -i $HBP/Experiments/GoogleGrasping/experiment_configuration.exc
+
 echo "done copying"
+
+# Create Symbolic Links for Models
 if [ "$1" = "" ] || [ "$1" = "m" ] ; then
  echo "setting up models"
  cd $HBP/Models
  ./create-symlinks.sh
 fi
-#
+
+# Re-building GazeboRosPackages
 if [ "$1" = "" ] || [ "$1" = "g" ] ; then
  echo "setting up packages"
  cd $HBP/GazeboRosPackages
  catkin_make
 fi
 
-echo "Experiment succesfully deployed"
+echo "Experiment successfully deployed"
 
